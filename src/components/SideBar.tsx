3255c8fb34d081b68e725ce095367cca05d7e4ee
caiokafnet <78740155+martins4duarte@users.noreply.github.com>
2022-11-04 14:12:38 -0300

    desafio

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,44 @@
+import { memo } from "react";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface IProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+function SideBarComponent({
+  genres,
+  selectedGenreId,
+  handleClickButton,
+}: IProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export const SideBar = memo(SideBarComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.genres, nextProps.genres);
+});
